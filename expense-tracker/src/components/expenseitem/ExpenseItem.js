f9089bafcf6cc9615744cb10c9c1d03daa6af0e4
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-10-25 11:30:30 -0400

    Passing props from the app component to the expense item component

--- expense-tracker/src/components/expenseitem/ExpenseItem.js
+++ expense-tracker/src/components/expenseitem/ExpenseItem.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import './ExpenseItem.css'
 
-const ExpenseItem = () => {
+const ExpenseItem = ({ date, title, amount }) => {
+
   return (
-    <>
-      <div>Date</div>
-      <h2>Title</h2>
-      <div>Amount</div>
-    </>
+    <div className='expense-item'>
+      <div>{date.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${amount}</div>
+      </div>
+    </div>
   )
 }
 
