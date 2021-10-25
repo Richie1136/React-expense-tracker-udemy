b91c5e331478505f68db46297454b7556a567fe4
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-10-25 11:58:30 -0400

    Destructing out the attributes of the expenses array

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -21,6 +21,12 @@ function App() {
       amount: 450,
       date: new Date(2021, 5, 12),
     },
+    {
+      id: 'e5',
+      title: 'New Monitor',
+      amount: 250,
+      date: new Date(2021, 10, 15),
+    },
   ]
   return (
     <div className="App">
@@ -29,11 +35,6 @@ function App() {
         const { title, amount, date } = expense
         return <ExpenseItem title={title} amount={amount} date={date} />
       })}
-      {/* <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} /> */}
-
     </div>
   );
 }
