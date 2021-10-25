d262d523faaa6405efee3a11bef7134523859f6d
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-10-25 10:23:02 -0400

    Starting on first component

---
+++ expense-tracker/src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/expenseitem/ExpenseItem";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>React expense tracker</h2>
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
