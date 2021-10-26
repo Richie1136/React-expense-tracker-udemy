2faa2b3e08da392c66c247bcf961e032146f9f38
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-10-26 11:45:23 -0400

    Moved everything related to the date inside a different component and then added that component inside of the expenseitem component

--- expense-tracker/src/components/expenseitem/ExpenseItem.js
+++ expense-tracker/src/components/expenseitem/ExpenseItem.js
@@ -1,18 +1,12 @@
 import React from 'react'
 import './ExpenseItem.css'
+import Date from '../date/Date'
 
 const ExpenseItem = ({ date, title, amount }) => {
-  const month = date.toLocaleString('en-US', { month: 'long' })
-  const year = date.getFullYear()
-  const day = date.toLocaleString('en-US', { day: '2-digit' })
 
   return (
     <div className='expense-item'>
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <Date date={date} />
       <div className='expense-item__description'>
         <h2>{title}</h2>
         <div className='expense-item__price'>${amount}</div>
