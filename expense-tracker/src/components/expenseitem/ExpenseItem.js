0b0edca50c2842cacbf9bb7a3290eeb88e0e22bb
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-10-26 14:11:57 -0400

    Destructing out the attributes of the props

--- expense-tracker/src/components/expenseitem/ExpenseItem.js
+++ expense-tracker/src/components/expenseitem/ExpenseItem.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import './ExpenseItem.css'
 import Date from '../date/Date'
+import Card from '../card/Card'
 
 const ExpenseItem = ({ date, title, amount }) => {
 
   return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
       <Date date={date} />
       <div className='expense-item__description'>
         <h2>{title}</h2>
         <div className='expense-item__price'>${amount}</div>
       </div>
-    </div>
+    </Card>
   )
 }
 
