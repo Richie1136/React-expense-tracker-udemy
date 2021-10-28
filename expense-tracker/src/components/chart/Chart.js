70b86fddb1f6114f96646f0a2d3b14694029a5df
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-10-28 14:06:24 -0400

    Working on the chart

---
+++ expense-tracker/src/components/chart/Chart.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Chart.css'
+import ChartBar from '../chartbar/ChartBar'
+
+const Chart = () => {
+
+  return (
+    <div className='chart'>
+      <ChartBar />
+    </div>
+  )
+}
+
+export default Chart
